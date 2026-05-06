54ca64570b9568361c1fe70c4aafdb49fc0f0240
muzamalms-hue <muzamalms@gmail.com>
2026-05-06 20:58:13 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,20 @@
+import { useState } from "react";
+import AcidBase from "./components/AcidBase";
+import Molarity from "./components/Molarity";
+import StockDilution from "./components/StockDilution";
+import BottomNav from "./components/BottomNav";
+import "./style.css";
 
+export default function App() {
+  const [page, setPage] = useState("acid");
+
+  return (
+    <>
+      {page === "acid" && <AcidBase />}
+      {page === "molarity" && <Molarity />}
+      {page === "stock" && <StockDilution />}
+
+      <BottomNav setPage={setPage} />
+    </>
+  );
+}
