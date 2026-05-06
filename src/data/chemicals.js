4ae95f3b9165aceceb667441bae7aad117901cae
muzamalms-hue <muzamalms@gmail.com>
2026-05-07 00:18:38 +0500

    Update chemicals.js

--- src/data/chemicals.js
+++ src/data/chemicals.js
@@ -1,5 +1,800 @@
 export default [
-  { name: "Hydrochloric Acid", density: 1.19, fw: 36.46, percentage: 37 },
-  { name: "Nitric Acid", density: 1.41, fw: 63.01, percentage: 70 },
-  { name: "Sulfuric Acid", density: 1.84, fw: 98.08, percentage: 98 }
+  {
+    name: "Hydrochloric Acid (HCl)",
+    density: 1.19,
+    mw: 36.46,
+    percentage: 37,
+    nFactor: 1
+  },
+
+  {
+    name: "Sulfuric Acid (H2SO4)",
+    density: 1.84,
+    mw: 98.08,
+    percentage: 98,
+    nFactor: 2
+  },
+
+  {
+    name: "Nitric Acid (HNO3)",
+    density: 1.41,
+    mw: 63.01,
+    percentage: 70,
+    nFactor: 1
+  },
+
+  {
+    name: "Phosphoric Acid (H3PO4)",
+    density: 1.88,
+    mw: 98.00,
+    percentage: 85,
+    nFactor: 3
+  },
+
+  {
+    name: "Hydrofluoric Acid (HF)",
+    density: 1.15,
+    mw: 20.01,
+    percentage: 48,
+    nFactor: 1
+  },
+
+  {
+    name: "Hydrobromic Acid (HBr)",
+    density: 1.49,
+    mw: 80.91,
+    percentage: 48,
+    nFactor: 1
+  },
+
+  {
+    name: "Hydroiodic Acid (HI)",
+    density: 1.70,
+    mw: 127.91,
+    percentage: 57,
+    nFactor: 1
+  },
+
+  {
+    name: "Perchloric Acid (HClO4)",
+    density: 1.67,
+    mw: 100.46,
+    percentage: 70,
+    nFactor: 1
+  },
+
+  {
+    name: "Chloric Acid (HClO3)",
+    density: 1.28,
+    mw: 84.46,
+    percentage: 30,
+    nFactor: 1
+  },
+
+  {
+    name: "Hypochlorous Acid (HClO)",
+    density: 1.11,
+    mw: 52.46,
+    percentage: 5,
+    nFactor: 1
+  },
+
+  {
+    name: "Chlorous Acid (HClO2)",
+    density: 1.20,
+    mw: 68.46,
+    percentage: 30,
+    nFactor: 1
+  },
+
+  {
+    name: "Carbonic Acid (H2CO3)",
+    density: 1.00,
+    mw: 62.03,
+    percentage: 5,
+    nFactor: 2
+  },
+
+  {
+    name: "Boric Acid (H3BO3)",
+    density: 1.44,
+    mw: 61.83,
+    percentage: 99,
+    nFactor: 3
+  },
+
+  {
+    name: "Chromic Acid (H2CrO4)",
+    density: 1.20,
+    mw: 118.01,
+    percentage: 50,
+    nFactor: 2
+  },
+
+  {
+    name: "Dichromic Acid (H2Cr2O7)",
+    density: 1.50,
+    mw: 218.00,
+    percentage: 50,
+    nFactor: 2
+  },
+
+  {
+    name: "Hydrosulfuric Acid (H2S)",
+    density: 1.00,
+    mw: 34.08,
+    percentage: 10,
+    nFactor: 2
+  },
+
+  {
+    name: "Sulfurous Acid (H2SO3)",
+    density: 1.03,
+    mw: 82.07,
+    percentage: 6,
+    nFactor: 2
+  },
+
+  {
+    name: "Hydroselenic Acid (H2Se)",
+    density: 1.00,
+    mw: 80.98,
+    percentage: 10,
+    nFactor: 2
+  },
+
+  {
+    name: "Hydrocyanic Acid (HCN)",
+    density: 0.69,
+    mw: 27.03,
+    percentage: 2,
+    nFactor: 1
+  },
+
+  {
+    name: "Nitrous Acid (HNO2)",
+    density: 1.00,
+    mw: 47.01,
+    percentage: 10,
+    nFactor: 1
+  },
+  
+{
+  name: "Pyrophosphoric Acid (H4P2O7)",
+  density: 2.04,
+  mw: 177.97,
+  percentage: 85,
+  nFactor: 4
+},
+
+{
+  name: "Permanganic Acid (HMnO4)",
+  density: 1.00,
+  mw: 119.94,
+  percentage: 5,
+  nFactor: 1
+},
+
+{
+  name: "Silicic Acid (H2SiO3)",
+  density: 1.00,
+  mw: 78.10,
+  percentage: 10,
+  nFactor: 2
+},
+
+{
+  name: "Arsenic Acid (H3AsO4)",
+  density: 1.87,
+  mw: 141.94,
+  percentage: 75,
+  nFactor: 3
+},
+
+{
+  name: "Selenic Acid (H2SeO4)",
+  density: 2.60,
+  mw: 144.97,
+  percentage: 95,
+  nFactor: 2
+},
+
+{
+  name: "Selenous Acid (H2SeO3)",
+  density: 1.00,
+  mw: 128.97,
+  percentage: 10,
+  nFactor: 2
+},
+
+{
+  name: "Telluric Acid (H6TeO6)",
+  density: 3.07,
+  mw: 229.64,
+  percentage: 99,
+  nFactor: 6
+},
+
+{
+  name: "Periodic Acid (HIO4)",
+  density: 1.00,
+  mw: 191.91,
+  percentage: 50,
+  nFactor: 1
+},
+
+{
+  name: "Metaphosphoric Acid (HPO3)",
+  density: 2.00,
+  mw: 79.98,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Orthophosphoric Acid (H3PO4)",
+  density: 1.88,
+  mw: 98.00,
+  percentage: 85,
+  nFactor: 3
+},
+
+{
+  name: "Polyphosphoric Acid",
+  density: 2.00,
+  mw: 115.00,
+  percentage: 85,
+  nFactor: 1
+},
+
+{
+  name: "Fluoroboric Acid (HBF4)",
+  density: 1.32,
+  mw: 87.81,
+  percentage: 48,
+  nFactor: 1
+},
+
+{
+  name: "Hexafluorosilicic Acid (H2SiF6)",
+  density: 1.22,
+  mw: 144.09,
+  percentage: 35,
+  nFactor: 2
+},
+
+{
+  name: "Hydrazoic Acid (HN3)",
+  density: 1.09,
+  mw: 43.03,
+  percentage: 5,
+  nFactor: 1
+},
+
+{
+  name: "Thiosulfuric Acid (H2S2O3)",
+  density: 1.00,
+  mw: 114.14,
+  percentage: 10,
+  nFactor: 2
+},
+
+{
+  name: "Acetic Acid (CH3COOH)",
+  density: 1.05,
+  mw: 60.05,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Formic Acid (HCOOH)",
+  density: 1.22,
+  mw: 46.03,
+  percentage: 88,
+  nFactor: 1
+},
+
+{
+  name: "Citric Acid",
+  density: 1.66,
+  mw: 192.12,
+  percentage: 99,
+  nFactor: 3
+},
+
+{
+  name: "Lactic Acid",
+  density: 1.21,
+  mw: 90.08,
+  percentage: 88,
+  nFactor: 1
+},
+
+{
+  name: "Oxalic Acid",
+  density: 1.90,
+  mw: 90.03,
+  percentage: 99,
+  nFactor: 2
+},
+
+{
+  name: "Tartaric Acid",
+  density: 1.79,
+  mw: 150.09,
+  percentage: 99,
+  nFactor: 2
+},
+
+{
+  name: "Malic Acid",
+  density: 1.61,
+  mw: 134.09,
+  percentage: 99,
+  nFactor: 2
+},
+
+{
+  name: "Succinic Acid",
+  density: 1.56,
+  mw: 118.09,
+  percentage: 99,
+  nFactor: 2
+},
+
+{
+  name: "Benzoic Acid",
+  density: 1.27,
+  mw: 122.12,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Salicylic Acid",
+  density: 1.44,
+  mw: 138.12,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Ascorbic Acid (Vitamin C)",
+  density: 1.65,
+  mw: 176.12,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Propionic Acid",
+  density: 0.99,
+  mw: 74.08,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Butyric Acid",
+  density: 0.96,
+  mw: 88.11,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Valeric Acid",
+  density: 0.94,
+  mw: 102.13,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Caproic Acid",
+  density: 0.93,
+  mw: 116.16,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Caprylic Acid",
+  density: 0.91,
+  mw: 144.21,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Capric Acid",
+  density: 0.89,
+  mw: 172.26,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Lauric Acid",
+  density: 0.88,
+  mw: 200.32,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Myristic Acid",
+  density: 0.86,
+  mw: 228.37,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Palmitic Acid",
+  density: 0.85,
+  mw: 256.42,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Stearic Acid",
+  density: 0.84,
+  mw: 284.48,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Oleic Acid",
+  density: 0.89,
+  mw: 282.47,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Linoleic Acid",
+  density: 0.90,
+  mw: 280.45,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Linolenic Acid",
+  density: 0.91,
+  mw: 278.43,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Acrylic Acid",
+  density: 1.05,
+  mw: 72.06,
+  percentage: 99,
+  nFactor: 1
+},
+{
+  name: "Methacrylic Acid",
+  density: 1.02,
+  mw: 86.09,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Maleic Acid",
+  density: 1.59,
+  mw: 116.07,
+  percentage: 99,
+  nFactor: 2
+},
+
+{
+  name: "Fumaric Acid",
+  density: 1.64,
+  mw: 116.07,
+  percentage: 99,
+  nFactor: 2
+},
+
+{
+  name: "Glutamic Acid",
+  density: 1.54,
+  mw: 147.13,
+  percentage: 99,
+  nFactor: 2
+},
+
+{
+  name: "Aspartic Acid",
+  density: 1.66,
+  mw: 133.10,
+  percentage: 99,
+  nFactor: 2
+},
+
+{
+  name: "Aminoacetic Acid",
+  density: 1.61,
+  mw: 75.07,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Pyruvic Acid",
+  density: 1.27,
+  mw: 88.06,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Uric Acid",
+  density: 1.89,
+  mw: 168.11,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Tannic Acid",
+  density: 1.00,
+  mw: 1701.20,
+  percentage: 95,
+  nFactor: 1
+},
+
+{
+  name: "Gallic Acid",
+  density: 1.69,
+  mw: 170.12,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Acetylsalicylic Acid",
+  density: 1.40,
+  mw: 180.16,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Trifluoroacetic Acid",
+  density: 1.49,
+  mw: 114.02,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Trichloroacetic Acid",
+  density: 1.63,
+  mw: 163.39,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Phenylacetic Acid",
+  density: 1.09,
+  mw: 136.15,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Glycolic Acid",
+  density: 1.49,
+  mw: 76.05,
+  percentage: 70,
+  nFactor: 1
+},
+
+{
+  name: "Mandelic Acid",
+  density: 1.30,
+  mw: 152.15,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Phthalic Acid",
+  density: 1.59,
+  mw: 166.13,
+  percentage: 99,
+  nFactor: 2
+},
+
+{
+  name: "Adipic Acid",
+  density: 1.36,
+  mw: 146.14,
+  percentage: 99,
+  nFactor: 2
+},
+
+{
+  name: "Sebacic Acid",
+  density: 1.21,
+  mw: 202.25,
+  percentage: 99,
+  nFactor: 2
+},
+
+{
+  name: "Nicotinic Acid",
+  density: 1.47,
+  mw: 123.11,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Folic Acid",
+  density: 1.68,
+  mw: 441.40,
+  percentage: 99,
+  nFactor: 2
+},
+
+{
+  name: "Pantothenic Acid",
+  density: 1.27,
+  mw: 219.23,
+  percentage: 98,
+  nFactor: 1
+},
+
+{
+  name: "Deoxycholic Acid",
+  density: 1.13,
+  mw: 392.57,
+  percentage: 98,
+  nFactor: 1
+},
+
+{
+  name: "Cholic Acid",
+  density: 1.28,
+  mw: 408.57,
+  percentage: 98,
+  nFactor: 1
+},
+
+{
+  name: "Taurocholic Acid",
+  density: 1.20,
+  mw: 515.69,
+  percentage: 95,
+  nFactor: 1
+},
+
+{
+  name: "Magic Acid",
+  density: 1.77,
+  mw: 147.07,
+  percentage: 100,
+  nFactor: 1
+},
+
+{
+  name: "Fluoroantimonic Acid",
+  density: 2.90,
+  mw: 236.75,
+  percentage: 100,
+  nFactor: 1
+},
+
+{
+  name: "Carborane Acid",
+  density: 1.00,
+  mw: 144.11,
+  percentage: 95,
+  nFactor: 1
+},
+
+{
+  name: "Methanesulfonic Acid",
+  density: 1.48,
+  mw: 96.10,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "p-Toluenesulfonic Acid",
+  density: 1.24,
+  mw: 172.20,
+  percentage: 98,
+  nFactor: 1
+},
+
+{
+  name: "Benzenesulfonic Acid",
+  density: 1.32,
+  mw: 158.17,
+  percentage: 98,
+  nFactor: 1
+},
+
+{
+  name: "Camphorsulfonic Acid",
+  density: 1.29,
+  mw: 232.30,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Arachidic Acid",
+  density: 0.85,
+  mw: 312.53,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Behenic Acid",
+  density: 0.86,
+  mw: 340.58,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Erucic Acid",
+  density: 0.86,
+  mw: 338.57,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Pelargonic Acid",
+  density: 0.91,
+  mw: 158.24,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Undecylenic Acid",
+  density: 0.91,
+  mw: 184.28,
+  percentage: 99,
+  nFactor: 1
+},
+
+{
+  name: "Humic Acid",
+  density: 1.00,
+  mw: 500.00,
+  percentage: 90,
+  nFactor: 1
+},
+
+{
+  name: "Fulvic Acid",
+  density: 1.00,
+  mw: 308.20,
+  percentage: 90,
+  nFactor: 1
+},
+
+{
+  name: "Nucleic Acid",
+  density: 1.70,
+  mw: 330.00,
+  percentage: 95,
+  nFactor: 1
+  }
 ];
