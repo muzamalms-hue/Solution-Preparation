20b1177de10dd3f2fefe8693193045164e5aed6d
muzamalms-hue <muzamalms@gmail.com>
2026-05-06 22:06:35 +0500

    Add chemicals data to chemicals.js

---
+++ src/data/chemicals.js
@@ -0,0 +1,12 @@
+export const chemicals = [
+  { name: "Acetic Acid", density: 1.049, fw: 60.05, wp: 99.7 },
+  { name: "Ammonium Hydroxide", density: 0.91, fw: 35.05, wp: 30 },
+  { name: "Formic Acid", density: 1.22, fw: 46.03, wp: 85 },
+  { name: "Hydrochloric Acid", density: 1.19, fw: 36.46, wp: 37 },
+  { name: "Hydrofluoric Acid", density: 1.15, fw: 20.01, wp: 48 },
+  { name: "Nitric Acid", density: 1.41, fw: 63.01, wp: 68 },
+  { name: "Perchloric Acid", density: 1.67, fw: 100.46, wp: 70 },
+  { name: "Phosphoric Acid", density: 1.88, fw: 98.00, wp: 85 },
+  { name: "Potassium Hydroxide", density: 2.12, fw: 56.11, wp: 100 },
+  { name: "Sodium Hydroxide", density: 2.13, fw: 40.00, wp: 100 }
+];
