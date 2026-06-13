fe90e3c3a1ef93f011b2cd5a26d4c37b3b6dfc2d
muzamalms-hue <muzamalms@gmail.com>
2026-06-14 04:40:48 +0500

    Update Volumetric.js

--- src/data/Volumetric.js
+++ src/data/Volumetric.js
@@ -4,174 +4,91 @@ export default [
   strength: "2 N",
 
   formula:
-    "N = (mL NaOH × N NaOH) / mL acetic acid",
+    "N = mL NaOH × N NaOH / mL acetic acid",
 
   preparation:
-    "Transfer 116 mL of glacial acetic acid to a 1000-mL volumetric flask containing about 500 mL of water. Cool and dilute with water to volume.",
+    "Transfer 116 mL of glacial acetic acid to 1000-mL volumetric flask containing about 500 mL of water. Cool and dilute with water to volume.",
 
   standardization:
     "Accurately transfer 10 mL of the 2 N acetic acid solution to a beaker. Add 50 mL of carbon dioxide-free water. Titrate potentiometrically with 1 N sodium hydroxide VS."
 },
-
   {
   name: "0.2 N Alcoholic Potassium Hydroxide VS",
   strength: "0.2 N",
 
+  formula:
+    "N = mL HCl × N HCl / mL KOH",
+
   preparation:
-    "Dissolve about 14 g of potassium hydroxide in 20 mL of water, and add aldehyde-free alcohol to make 1000 mL. Allow the solution to stand in a tightly stoppered bottle for 24 h. Then quickly decant the clear supernatant into a suitable, tight container. Store in tightly stoppered bottles, protected from light.",
+    "11.22 g in 1000 mL. Dissolve about 14 g of potassium hydroxide in 20 mL of water, and add aldehyde-free alcohol to make 1000 mL. Allow the solution to stand in a tightly stoppered bottle for 24 h. Then quickly decant the clear supernatant into a suitable, tight container. Store in tightly stoppered bottles, protected from light.",
 
   standardization:
-    "Accurately measure 10 mL of 0.5 N hydrochloric acid VS. Dilute with 50 mL of water, add 2 drops of phenolphthalein TS, and titrate with the alcoholic potassium hydroxide solution until a permanent pale pink color is produced.",
-
-  formula:
-    "N = (mL HCl × N HCl) / mL KOH"
-  },
+    "Accurately measure 10 mL of 0.5 N hydrochloric acid VS. Dilute with 50 mL of water, add 2 drops of phenolphthalein TS, and titrate with the alcoholic potassium hydroxide solution until a permanent pale pink color is produced."
+},
   {
   name: "0.1 N Ammonium Thiocyanate VS",
   strength: "0.1 N",
 
+  formula:
+    "N = mL AgNO3 × N AgNO3 / mL NH4SCN Solution",
+
   preparation:
     "Dissolve about 8 g of ammonium thiocyanate in 1000 mL of water.",
 
-  standardizationVisualEndpoint:
-    "Accurately measure about 30 mL of 0.1 N silver nitrate VS into a glass-stoppered flask. Dilute with 50 mL of water, then add 2 mL of nitric acid and 2 mL of ferric ammonium sulfate TS, and titrate with the ammonium thiocyanate solution to the first appearance of a red-brown color.",
-
-  standardizationPotentiometricEndpoint:
-    "Accurately measure about 10 mL of 0.1 N silver nitrate VS into a glass-stoppered flask. Dilute with 50 mL of water, add 2 mL of nitric acid, and titrate potentiometrically with the ammonium thiocyanate solution using a silver indicating electrode and an appropriate reference electrode or a combined silver electrode.",
-
-  formula:
-    "N = (mL AgNO3 × N AgNO3) / mL NH4SCN Solution"
-  },
-
+  standardization:
+    "Standardization with visual endpoint: Accurately measure about 30 mL of 0.1 N silver nitrate VS into a glass-stoppered flask. Dilute with 50 mL of water, then add 2 mL of nitric acid and 2 mL of ferric ammonium sulfate TS, and titrate with the ammonium thiocyanate solution to the first appearance of a red-brown color. If desirable, 0.1 N ammonium thiocyanate may be replaced by 0.1 N potassium thiocyanate where the former is directed in various tests and assays. Standardization with potentiometric endpoint: Accurately measure about 10 mL of 0.1 N silver nitrate VS into a glass-stoppered flask. Dilute with 50 mL of water, add 2 mL of nitric acid, and titrate potentiometrically with the ammonium thiocyanate solution using a silver indicating electrode and an appropriate reference electrode or a combined silver electrode. If desirable, 0.1 N ammonium thiocyanate may be replaced by 0.1 N potassium thiocyanate where the former is directed in various tests and assays."
+},
   {
   name: "0.05 M Barium Perchlorate VS",
   strength: "0.05 M",
 
-  dilutedAceticAcidSolution:
-    "Transfer 28.5 mL of glacial acetic acid to a 100-mL volumetric flask. Dilute with water to volume.",
-
-  dilutedAmmoniaSolution:
-    "Transfer 75 mL of stronger ammonia water to a 100-mL volumetric flask. Dilute with water to volume.",
-
-  bufferSolutionPH37:
-    "Transfer 15.0 mL of Diluted acetic acid solution to a 100-mL volumetric flask. Add 60 mL of alcohol and 20 mL of water. Adjust with Diluted ammonia solution to a pH of 3.7. Dilute with water to volume.",
+  formula:
+    "M = mL sulfuric acid × N sulfuric acid × 0.5 / mL barium perchlorate\nM = mL edetate solution × N edetate solution / mL barium perchlorate",
 
   preparation:
-    "Dissolve 15.8 g of barium hydroxide in a mixture of 7.5 mL of perchloric acid and 75 mL of water. Adjust with perchloric acid to a pH of 3 and filter if necessary. Add 150 mL of alcohol and dilute with water to 250 mL. Dilute with Buffer solution pH 3.7 to 1000 mL.",
+    "Diluted acetic acid solution: Transfer 28.5 mL of glacial acetic acid to a 100-mL volumetric flask. Dilute with water to volume. Diluted ammonia solution: Transfer 75 mL of stronger ammonia water to a 100-mL volumetric flask. Dilute with water to volume. Buffer solution pH 3.7: Transfer 15.0 mL of Diluted acetic acid solution to a 100-mL volumetric flask. Add 60 mL of alcohol and 20 mL of water. Adjust with Diluted ammonia solution to a pH of 3.7. Dilute with water to volume. Barium perchlorate solution: Dissolve 15.8 g of barium hydroxide in a mixture of 7.5 mL of perchloric acid and 75 mL of water. Adjust with perchloric acid to a pH of 3 and filter if necessary. Add 150 mL of alcohol and dilute with water to 250 mL. Dilute with Buffer solution pH 3.7 to 1000 mL.",
 
-  standardizationVisualEndpoint:
-    "Transfer 10 mL of 1 N sulfuric acid VS to a 100 mL volumetric flask and dilute to volume with water. To 5.0 mL of 0.1 N sulfuric acid add 5 mL of water, 50 mL of Buffer solution pH 3.7, and 0.5 mL of sodium alizarinsulfonate TS. Titrate with the Barium perchlorate solution until an orange-red color appears. Standardize immediately before use.",
-
-  formulaVisualEndpoint:
-    "M = (mL sulfuric acid × N sulfuric acid × 0.5) / mL barium perchlorate",
-
-  ammoniaBufferSolutionPH10:
-    "Transfer 54 g of ammonium chloride to a 1000 mL volumetric flask containing about 200 mL of water, swirl until dissolved. Add 350 mL of 25% ammonia water. Dilute to volume with water.",
-
-  standardizationPotentiometricEndpoint:
-    "To 50 mL of water, add 10.0 mL of 0.05 M barium perchlorate solution, 5 mL of Ammonia buffer solution pH 10 and 1 mL of Copper–EDTA complex solution. Titrate potentiometrically with 0.05 M edetate disodium VS using a copper ion-selective electrode. Standardize immediately before use.",
-
-  formulaPotentiometricEndpoint:
-    "M = (mL edetate solution × N edetate solution) / mL barium perchlorate"
-  },
-
+  standardization:
+    "Standardization with visual endpoint: 0.1 N sulfuric acid: Transfer 10 mL of 1 N sulfuric acid VS to a 100 mL volumetric flask and dilute to volume with water. To 5.0 mL of 0.1 N sulfuric acid add 5 mL of water, 50 mL of Buffer solution pH 3.7, and 0.5 mL of sodium alizarinsulfonate TS. Titrate with the Barium perchlorate solution until an orange-red color appears. Standardize immediately before use. Standardization with potentiometric endpoint: Ammonia buffer solution pH 10: Transfer 54 g of ammonium chloride to a 1000 mL volumetric flask containing about 200 mL of water, swirl until dissolved. Add 350 mL of 25% ammonia water. Dilute to volume with water. To 50 mL of water, add 10.0 mL of 0.05 M barium perchlorate solution, 5 mL of Ammonia buffer solution pH 10 and 1 mL of Copper–EDTA complex solution. Titrate potentiometrically with 0.05 M edetate disodium VS using a copper ion-selective electrode. Standardize immediately before use."
+},
   {
-  name: "0.004 M Benzethonium Chloride VS",
+  name: "0.004 M Benzethonium Chloride",
   strength: "0.004 M",
-
-  preparation:
-    "Dissolve 1.792 g of benzethonium chloride, previously dried at 100°–105° to constant weight, in water to make 1000 mL.",
 
   formula:
     "M = (mL NaB(C6H5)4 × M NaB(C6H5)4) / mL benzethonium chloride",
 
-  standardization:
-    "Visual endpoint: Dissolve 0.350 g of the dried benzethonium chloride in 30 mL of glacial acetic acid and add 6 mL of mercuric acetate TS. Titrate with 0.1 N perchloric acid in glacial acetic acid using crystal violet TS. Potentiometric endpoint: Accurately measure 2.0 mL of standardized 0.02 M sodium tetraphenylboron VS, add 50 mL water and 1 mL of 1 N sodium hydroxide VS, then titrate with the benzethonium chloride solution."
-},
+  preparation:
+    "Dissolve 1.792 g of benzethonium chloride, previously dried at 100°–105° to constant weight, in water to make 1000 mL.",
 
-{
+  standardization:
+    "Standardization with visual endpoint: Dissolve 0.350 g of the dried benzethonium chloride in 30 mL of glacial acetic acid and add 6 mL of mercuric acetate TS. Titrate with 0.1 N perchloric acid in glacial acetic acid VS, using 0.05 mL of crystal violet TS as an indicator. Carry out a blank titration. One mL of 0.1 N perchloric acid VS is equivalent to 44.81 mg of benzethonium chloride (C27H42ClNO2). Calculate the molarity of the volumetric solution from the content of benzethonium chloride in the dried benzethonium chloride, as determined through the titration above. Standardization with potentiometric endpoint: Accurately measure 2.0 mL of standardized 0.02 M sodium tetraphenylboron VS into a titration vessel, add 50 mL of water and 1 mL of 1 N sodium hydroxide VS. Titrate with the benzethonium chloride solution using a surfactant electrode suitable for non-ionic surfactants."
+},
+  {
   name: "0.01 M Bismuth Nitrate VS",
   strength: "0.01 M",
+
+  formula:
+    "M = (mL EDTA × M EDTA) / mL bismuth nitrate\nM = ((mL EDTA × M EDTA) − (mL cupric nitrate × N cupric nitrate)) / mL bismuth nitrate",
 
   preparation:
     "Dissolve 4.86 g of bismuth nitrate pentahydrate in 60 mL of diluted nitric acid, and add 0.01 N nitric acid to make 1000 mL.",
 
-  formula:
-    "M = (mL EDTA × M EDTA) / mL bismuth nitrate",
-
   standardization:
-    "Visual endpoint: Measure 25 mL solution, add 50 mL water and xylenol orange TS, titrate with 0.01 M edetate disodium VS until red changes to yellow. Potentiometric endpoint: Use acetate buffer pH 4.7 and titrate with 0.1 N cupric nitrate VS using copper ion-selective electrode."
+    "Standardization with visual endpoint: Accurately measure 25 mL of the prepared bismuth nitrate solution, add 50 mL of water and 1 drop of xylenol orange TS, and titrate the solution with 0.01 M edetate disodium VS until the red color changes to yellow. Standardization with potentiometric endpoint: Acetate buffer, pH 4.7: Transfer 116 g of ammonium acetate to a 1000-mL volumetric flask containing about 200 mL of water, and swirl until dissolved. Add 86 mL of glacial acetic acid and dilute with water to volume. Accurately measure 25 mL of the bismuth nitrate solution into a titration vessel, add 5 mL of Acetate buffer, pH 4.7 and 5 mL of 0.01 M edetate disodium VS. After a reaction time of 1 min, titrate potentiometrically with 0.1 N cupric nitrate VS using a copper ion-selective electrode."
 },
-
-{
+  {
   name: "0.1 N Bromine VS",
   strength: "0.1 N",
-
-  preparation:
-    "Dissolve 3 g of potassium bromate and 15 g of potassium bromide in water to make 1000 mL. Preserve in dark amber-colored, glass-stoppered bottles.",
 
   formula:
     "N = (mL Na2S2O3 × N Na2S2O3) / mL Br2 solution",
 
-  standardization:
-    "Liberate iodine with hydrochloric acid and potassium iodide TS and titrate with 0.1 N sodium thiosulfate VS using starch TS. Potentiometric standardization may be performed using a combined platinum electrode."
-},
-
-{
-  name: "0.05 N Ceric Ammonium Nitrate VS",
-  strength: "0.05 N",
-
   preparation:
-    "Dissolve 2.75 g of ceric ammonium nitrate in 1 N nitric acid to obtain 100 mL of solution, and filter.",
-
-  formula:
-    "N = (mL Fe(NH4)2(SO4)2 × N Fe(NH4)2(SO4)2) / (mL Ce(NO3)4·2NH4NO3)",
+    "Dissolve 3 g of potassium bromate and 15 g of potassium bromide in water to make 1000 mL. Preserve in dark amber-colored, glass-stoppered bottles.",
 
   standardization:
-    "Titrate freshly standardized 0.1 N ferrous ammonium sulfate VS with ceric ammonium nitrate solution using either nitrophenanthroline TS visual endpoint or potentiometric endpoint."
-},
-
-{
-  name: "0.1 N Ceric Ammonium Sulfate VS",
-  strength: "0.1 N",
-
-  preparation:
-    "Transfer 65 g of ceric ammonium sulfate to a 1000-mL volumetric flask containing a mixture of 30 mL of sulfuric acid and 500 mL of water. Swirl until dissolved. Cool and dilute with water to volume.",
-
-  formula:
-    "N = (mL Na2S2O3 × N Na2S2O3) / mL Ce(NH4)4(SO4)4",
-
-  standardization:
-    "Add potassium iodide and titrate with freshly standardized 0.1 N sodium thiosulfate VS using visual or potentiometric endpoint."
-},
-
-{
-  name: "0.01 N Ceric Ammonium Sulfate VS",
-  strength: "0.01 N",
-
-  preparation:
-    "Transfer 6.5 g of ceric ammonium sulfate to a 1000-mL volumetric flask containing a mixture of 30 mL of sulfuric acid and 500 mL of water. Swirl until dissolved. Cool and dilute with water to volume.",
-
-  formula:
-    "N = (mL Na2S2O3 × N Na2S2O3) / mL Ce(NH4)4(SO4)4",
-
-  standardization:
-    "Add potassium iodide and titrate immediately with freshly standardized 0.01 M sodium thiosulfate VS using visual or potentiometric endpoint."
-},
-
-{
-  name: "0.1 N Ceric Sulfate VS",
-  strength: "0.1 N",
-
-  preparation:
-    "Use commercially available volumetric standard solution.",
-
-  formula:
-    "N = mg Na2C2O4 / (67.00 × mL Ce(SO4)2 solution)",
-
-  standardization:
-    "Accurately weigh sodium oxalate primary standard, dissolve in water, add sulfuric acid and hydrochloric acid, heat to 70°–75°, and titrate with 0.1 N ceric sulfate to a permanent slight yellow color."
-},
-
+    "Standardization with visual endpoint: Accurately measure about 25 mL of the solution into a 500-mL iodine flask, and dilute with 120 mL of water. Add 5 mL of hydrochloric acid, insert the stopper in the flask, and shake it gently. Then add 5 mL of potassium iodide TS, again insert the stopper, shake the mixture, allow it to stand for 5 min, and titrate the liberated iodine with 0.1 N sodium thiosulfate VS, adding 3 mL of starch TS as the endpoint is approached. Standardization with potentiometric endpoint: Accurately measure 10 mL of the solution into a 500-mL iodine flask, and dilute with 120 mL of water. Add 2 mL of hydrochloric acid, insert the stopper in the flask, and shake it gently. Then add 2 mL of potassium iodide TS, again insert the stopper, shake the mixture, allow it to stand for 5 min, and titrate the liberated iodine potentiometrically with 0.1 N sodium thiosulfate VS using a combined platinum electrode."
+}
   
   ]
