5c6a761d85b406c9a2cffc12765ff721d24be4e0
muzamalms-hue <muzamalms@gmail.com>
2026-06-14 01:23:43 +0500

    Update BottomNav.jsx

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,68 +1,54 @@
 export default function BottomNav({
-  page,
-  setPage,
+page,
+setPage,
 }) {
-  return (
-    <div className="bottom-nav">
-
-      {/* ROW 1 */}
-      <div className="nav-row">
-        <button
-          className={
-            page === "acid"
-              ? "nav-btn active"
-              : "nav-btn"
-          }
-          onClick={() =>
-            setPage("acid")
-          }
-        >
-          Acid & Base
-        </button>
-
-        <button
-          className={
-            page === "molarity"
-              ? "nav-btn active"
-              : "nav-btn"
-          }
-          onClick={() =>
-            setPage("molarity")
-          }
-        >
-          Molarity
-        </button>
-
-        <button
-          className={
-            page === "stock"
-              ? "nav-btn active"
-              : "nav-btn"
-          }
-          onClick={() =>
-            setPage("stock")
-          }
-        >
-          Stock Dilution
-        </button>
-      </div>
-
-      {/* ROW 2 */}
-      <div className="nav-row">
-        <button
-          className={
-            page === "volumetric"
-              ? "nav-btn active"
-              : "nav-btn"
-          }
-          onClick={() =>
-            setPage("volumetric")
-          }
-        >
-          Volumetric
-        </button>
-      </div>
-
-    </div>
-  );
+return (
+<div className="bottom-nav">
+{/* ACID */}
+<button
+className={
+page === "acid"
+? "nav-btn active"
+: "nav-btn"
 }
+onClick={() =>
+setPage("acid")
+}
+>
+Acid & Base
+</button>
+
+{/* MOLARITY */}  
+  <button  
+    className={  
+      page === "molarity"  
+        ? "nav-btn active"  
+        : "nav-btn"  
+    }  
+    onClick={() =>  
+      setPage("molarity")  
+    }  
+  >  
+    Molarity  
+  </button>  
+
+  {/* STOCK */}  
+  <button  
+    className={  
+      page === "stock"  
+        ? "nav-btn active"  
+        : "nav-btn"  
+    }  
+    onClick={() =>  
+      setPage("stock")  
+    }  
+  >  
+    Stock Dilution  
+  </button>  
+</div>
+
+);
+}
+
+Ye is ka code hai ab 4th button nechy add krna hai ye already 3 thk hain 1 row me.
+Ab 2nd row me new button add kr do
