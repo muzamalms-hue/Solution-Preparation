f72e3a04881bc6fdccd8fd77bc9cfa06f92fa9f2
muzamalms-hue <muzamalms@gmail.com>
2026-06-14 15:09:26 +0500

    Update Solutions.jsx

--- src/components/Solutions.jsx
+++ src/components/Solutions.jsx
@@ -17,7 +17,7 @@ export default function Solutions() {
   return (
     <div className="container">
       <h2 className="title">
-        Solutions
+        Solutions 123
       </h2>
 
       <button
