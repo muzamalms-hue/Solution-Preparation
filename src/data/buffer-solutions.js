0028ded4b4a8bcd3cca90026d984a87d362b0b07
muzamalms-hue <muzamalms@gmail.com>
2026-06-14 18:56:06 +0500

    Update buffer-solutions.js

--- src/data/buffer-solutions.js
+++ src/data/buffer-solutions.js
@@ -1,1 +1,87 @@
+const bufferSolutions = [
+  {
+    name: "Acetate Buffer pH 2.45",
+    preparation:
+      "Mix 200 ml of 1M hydrochloric acid with 200 ml of 1M sodium acetate and dilute to 1000 ml with water. Adjust pH to 2.45 before use."
+  },
+  {
+    name: "Acetate Buffer pH 2.8",
+    preparation:
+      "Dissolve 4 g anhydrous sodium acetate in 840 ml water, add glacial acetic acid to pH 2.8 and dilute to 1000 ml."
+  },
+  {
+    name: "Acetate Buffer pH 3.4",
+    preparation:
+      "Mix 5 volumes of 0.1M sodium acetate with 95 volumes of 0.1M acetic acid."
+  },
+  {
+    name: "Acetate Buffer pH 3.5",
+    preparation:
+      "Dissolve 25 g ammonium acetate in water, add hydrochloric acid, adjust pH 3.5 and dilute to 100 ml."
+  },
+  {
+    name: "Acetate Buffer pH 3.7",
+    preparation:
+      "Dissolve 10 g sodium acetate in water, adjust pH 3.7 with acetic acid and dilute to 1000 ml."
+  },
+  {
+    name: "Acetate Buffer pH 4.4",
+    preparation:
+      "Dissolve sodium acetate and ammonium acetate in water, add glacial acetic acid and make to 1000 ml."
+  },
+  {
+    name: "Acetate Buffer pH 4.6",
+    preparation:
+      "Dissolve sodium acetate in water, add glacial acetic acid and dilute to 100 ml."
+  },
+  {
+    name: "Acetate Buffer pH 5.0",
+    preparation:
+      "Dissolve sodium acetate and acetic acid in water and dilute to 1000 ml."
+  },
+  {
+    name: "Acetate Buffer pH 6.0",
+    preparation:
+      "Dissolve ammonium acetate in water, add glacial acetic acid, adjust pH and dilute."
+  },
 
+  {
+    name: "Ammonia Buffer pH 10.0",
+    preparation:
+      "Dissolve ammonium chloride in water, add ammonia solution and dilute to 100 ml."
+  },
+  {
+    name: "Ammonium Carbonate Buffer pH 10.3",
+    preparation:
+      "Dissolve ammonium carbonate in water, adjust pH with sodium hydroxide and dilute to 1000 ml."
+  },
+  {
+    name: "Ammonium Chloride Buffer pH 9.5",
+    preparation:
+      "Dissolve ammonium chloride in water, add ammonia and dilute to 250 ml."
+  },
+
+  {
+    name: "Phosphate Buffer pH 7.4",
+    preparation:
+      "Dissolve potassium dihydrogen phosphate, disodium hydrogen phosphate and sodium chloride in water and adjust pH."
+  },
+  {
+    name: "Phosphate Buffer pH 6.8",
+    preparation:
+      "Mix disodium hydrogen phosphate and potassium dihydrogen phosphate in water and adjust pH."
+  },
+
+  {
+    name: "Tris Buffer pH 7.4",
+    preparation:
+      "Dissolve Tris(hydroxymethyl)aminomethane and sodium chloride in water and adjust pH with acid."
+  },
+  {
+    name: "Tris Buffer pH 8.0",
+    preparation:
+      "Dissolve Tris and sodium acetate in water, adjust pH and dilute to 1000 ml."
+  }
+];
+
+export default bufferSolutions;
