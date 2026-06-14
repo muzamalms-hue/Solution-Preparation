1172c87d02d9ed392fa4b6a6fae47a29df7e2303
muzamalms-hue <muzamalms@gmail.com>
2026-06-14 19:15:43 +0500

    Update test-solutions.js

--- src/data/test-solutions.js
+++ src/data/test-solutions.js
@@ -1,1 +1,296 @@
+// src/data/reagents.js (Test Solutions)
 
+const testSolutions = [
+  {
+    id: 1,
+    name: "Acetate Buffer TS",
+    prep: "Dissolve 320 g ammonium acetate in 500 mL water, add 5 mL glacial acetic acid, dilute to 1000 mL. pH 5.9–6.0"
+  },
+  {
+    id: 2,
+    name: "Acetic Acid–Ammonium Acetate Buffer TS",
+    prep: "Dissolve 77.1 g ammonium acetate in water, add 57 mL glacial acetic acid, dilute to 1000 mL"
+  },
+  {
+    id: 3,
+    name: "Ammonia–Ammonium Chloride Buffer TS",
+    prep: "Dissolve 67.5 g ammonium chloride in water, add 570 mL ammonium hydroxide, dilute to 1000 mL"
+  },
+  {
+    id: 4,
+    name: "Ammonia TS",
+    prep: "Contains 9.5–10.5% NH3. Dilute 350 mL strong ammonia water to 1000 mL"
+  },
+  {
+    id: 5,
+    name: "Ammoniacal Potassium Ferricyanide TS",
+    prep: "Dissolve 2 g potassium ferricyanide in 75 mL water, add 25 mL ammonium hydroxide"
+  },
+  {
+    id: 6,
+    name: "Ammonium Acetate TS",
+    prep: "Dissolve 10 g ammonium acetate in water to make 100 mL"
+  },
+  {
+    id: 7,
+    name: "Ammonium Carbonate TS",
+    prep: "Dissolve 20 g ammonium carbonate and 20 mL ammonia TS in water to 100 mL"
+  },
+  {
+    id: 8,
+    name: "Ammonium Chloride TS",
+    prep: "Dissolve 10.5 g ammonium chloride in water to make 100 mL"
+  },
+  {
+    id: 9,
+    name: "Ammonium Chloride–Ammonium Hydroxide TS",
+    prep: "Mix equal volumes water and ammonium hydroxide, saturate with ammonium chloride"
+  },
+  {
+    id: 10,
+    name: "Ammonium Oxalate TS",
+    prep: "Dissolve 3.5 g ammonium oxalate in water to make 100 mL"
+  },
+  {
+    id: 11,
+    name: "Ammonium Thiocyanate TS",
+    prep: "Dissolve 8 g ammonium thiocyanate in water to make 100 mL"
+  },
+  {
+    id: 12,
+    name: "Barium Chloride TS",
+    prep: "Dissolve 12 g barium chloride in water to make 100 mL"
+  },
+  {
+    id: 13,
+    name: "Barium Hydroxide TS",
+    prep: "Saturated solution in recently boiled water, prepare fresh"
+  },
+  {
+    id: 14,
+    name: "Barium Nitrate TS",
+    prep: "Dissolve 6.5 g barium nitrate in water to make 100 mL"
+  },
+  {
+    id: 15,
+    name: "Bromocresol Green TS",
+    prep: "Dissolve 50 mg bromocresol green in 100 mL alcohol, filter if needed"
+  },
+  {
+    id: 16,
+    name: "Bromocresol Green–Methyl Red TS",
+    prep: "Dissolve 0.15 g bromocresol green + 0.1 g methyl red in 180 mL alcohol, dilute to 200 mL"
+  },
+  {
+    id: 17,
+    name: "Bromothymol Blue TS",
+    prep: "Dissolve 100 mg bromothymol blue in 100 mL diluted alcohol"
+  },
+  {
+    id: 18,
+    name: "Calcium Chloride TS",
+    prep: "Dissolve 7.5 g calcium chloride in water to make 100 mL"
+  },
+  {
+    id: 19,
+    name: "Ceric Ammonium Nitrate TS",
+    prep: "Dissolve 6.25 g ceric ammonium nitrate in 10 mL 0.25N nitric acid, use within 3 days"
+  },
+  {
+    id: 20,
+    name: "Cresol Red TS",
+    prep: "Triturate 100 mg cresol red with 26.2 mL 0.01N NaOH, dilute to 250 mL"
+  },
+  {
+    id: 21,
+    name: "Cupric Acetate TS",
+    prep: "Dissolve 100 mg cupric acetate in water with few drops acetic acid, dilute to 100 mL"
+  },
+  {
+    id: 22,
+    name: "Cupric Sulfate TS",
+    prep: "Dissolve 12.5 g cupric sulfate in water to make 100 mL"
+  },
+  {
+    id: 23,
+    name: "Diphenylamine TS",
+    prep: "Dissolve 1 g diphenylamine in 100 mL sulfuric acid (colorless)"
+  },
+  {
+    id: 24,
+    name: "Diphenylcarbazone TS",
+    prep: "Dissolve 1 g diphenylcarbazone in 75 mL alcohol, dilute to 100 mL, store in brown bottle"
+  },
+  {
+    id: 25,
+    name: "Edetate Disodium TS",
+    prep: "Dissolve 1 g in 950 mL water, add 50 mL alcohol"
+  },
+  {
+    id: 26,
+    name: "Eosin Y TS",
+    prep: "Dissolve 50 mg eosin Y in 10 mL water"
+  },
+  {
+    id: 27,
+    name: "Eriochrome Black TS",
+    prep: "Dissolve 200 mg eriochrome black T + 2 g hydroxylamine HCl in methanol to 50 mL"
+  },
+  {
+    id: 28,
+    name: "Ferric Chloride TS",
+    prep: "Dissolve 9 g ferric chloride in water to make 100 mL"
+  },
+  {
+    id: 29,
+    name: "Ferrous Sulfate TS",
+    prep: "Dissolve 8 g ferrous sulfate in recently boiled cooled water, prepare fresh"
+  },
+  {
+    id: 30,
+    name: "Iodine Diluted TS",
+    prep: "Dilute 10 mL 0.1N iodine VS to 100 mL"
+  },
+  {
+    id: 31,
+    name: "Iodine + Potassium Iodide TS 1",
+    prep: "Dissolve 500 mg iodine + 1.5 g KI in 25 mL water"
+  },
+  {
+    id: 32,
+    name: "Iodine + Potassium Iodide TS 2",
+    prep: "Prepare iodine/KI solution, dilute as specified, prepare fresh"
+  },
+  {
+    id: 33,
+    name: "Iodobromide TS",
+    prep: "Dissolve iodine monobromide in glacial acetic acid to 1000 mL"
+  },
+  {
+    id: 34,
+    name: "Iodochloride TS",
+    prep: "Dissolve iodine monochloride in glacial acetic acid to 1000 mL"
+  },
+  {
+    id: 35,
+    name: "Lead Acetate TS",
+    prep: "Dissolve lead acetate crystals in water or alcohol to 100 mL"
+  },
+  {
+    id: 36,
+    name: "Magnesium Sulfate TS",
+    prep: "Dissolve 12 g magnesium sulfate in water to make 100 mL"
+  },
+  {
+    id: 37,
+    name: "Mercuric Acetate TS",
+    prep: "Dissolve 6 g mercuric acetate in glacial acetic acid to 100 mL"
+  },
+  {
+    id: 38,
+    name: "Mercuric Chloride TS",
+    prep: "Dissolve 6.5 g mercuric chloride in water to 100 mL"
+  },
+  {
+    id: 39,
+    name: "Methyl Orange TS",
+    prep: "Dissolve 100 mg methyl orange in 100 mL water"
+  },
+  {
+    id: 40,
+    name: "Methyl Red TS",
+    prep: "Dissolve 100 mg methyl red in 100 mL alcohol"
+  },
+  {
+    id: 41,
+    name: "Phenolphthalein TS",
+    prep: "Dissolve 10 g phenolphthalein in 100 mL alcohol"
+  },
+  {
+    id: 42,
+    name: "Potassium Chromate TS",
+    prep: "Dissolve 10 g potassium chromate in water to 100 mL"
+  },
+  {
+    id: 43,
+    name: "Potassium Dichromate TS",
+    prep: "Dissolve 7.5 g potassium dichromate in water to 100 mL"
+  },
+  {
+    id: 44,
+    name: "Potassium Ferricyanide TS",
+    prep: "Dissolve 1 g in 10 mL water, prepare fresh"
+  },
+  {
+    id: 45,
+    name: "Potassium Ferrocyanide TS",
+    prep: "Dissolve 1 g in 10 mL water, prepare fresh"
+  },
+  {
+    id: 46,
+    name: "Potassium Hydroxide TS",
+    prep: "Dissolve 6.5 g KOH in water to 100 mL"
+  },
+  {
+    id: 47,
+    name: "Potassium Iodide TS",
+    prep: "Dissolve 16.5 g KI in water to 100 mL"
+  },
+  {
+    id: 48,
+    name: "Potassium Iodide + Starch TS",
+    prep: "Prepare starch + KI solution, boil and cool"
+  },
+  {
+    id: 49,
+    name: "Silver Nitrate TS",
+    prep: "Use 0.1N silver nitrate solution"
+  },
+  {
+    id: 50,
+    name: "Sodium Acetate TS",
+    prep: "Dissolve 13.6 g sodium acetate in water to 100 mL"
+  },
+  {
+    id: 51,
+    name: "Sodium Carbonate TS",
+    prep: "Dissolve 10.6 g sodium carbonate in water to 100 mL"
+  },
+  {
+    id: 52,
+    name: "Sodium Hydroxide TS",
+    prep: "Dissolve 4 g NaOH in water to 100 mL"
+  },
+  {
+    id: 53,
+    name: "Sodium Hydroxide TS 2",
+    prep: "Dissolve 8.5 g NaOH in water to 100 mL volumetric flask"
+  },
+  {
+    id: 54,
+    name: "Sodium Sulfide TS",
+    prep: "Dissolve 1 g sodium sulfide in 10 mL water, prepare fresh"
+  },
+  {
+    id: 55,
+    name: "Starch TS",
+    prep: "Prepare starch paste with boiling water, use clear solution"
+  },
+  {
+    id: 56,
+    name: "Sulfanilic Acid TS",
+    prep: "Dissolve 800 mg sulfanilic acid in 100 mL acetic acid"
+  },
+  {
+    id: 57,
+    name: "Thioacetamide TS",
+    prep: "Dissolve 4 g thioacetamide in 100 mL water"
+  },
+  {
+    id: 58,
+    name: "Xylenol Orange TS",
+    prep: "Dissolve 100 mg in 100 mL alcohol"
+  }
+];
+
+export default testSolutions;
