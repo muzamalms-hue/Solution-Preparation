86a9b485c013e3a07017c4a8d8b13602c7df5548
muzamalms-hue <muzamalms@gmail.com>
2026-06-14 15:21:09 +0500

    Update StandardSolutions.jsx

--- src/components/StandardSolutions.jsx
+++ src/components/StandardSolutions.jsx
@@ -3,9 +3,11 @@ import standardSolutions from "../data/standard-solutions";
 export default function StandardSolutions() {
   return (
     <div>
-      <h1>Standard Solutions Working</h1>
+      <h1>Standard Solutions</h1>
 
-      <p>Total Records: {standardSolutions.length}</p>
+      {standardSolutions.map((item, index) => (
+        <p key={index}>{item.name}</p>
+      ))}
     </div>
   );
 }
