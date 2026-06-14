652b05e5a1723df131287a10e8afd68693c61976
muzamalms-hue <muzamalms@gmail.com>
2026-06-14 18:11:28 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,45 +4,35 @@ import AcidBase from "./components/AcidBase";
 import Molarity from "./components/Molarity";
 import StockDilution from "./components/StockDilution";
 import Solutions from "./components/Solutions";
+import Indicators from "./components/Indicators";
 
 import BottomNav from "./components/BottomNav";
 
 function App() {
-// ACTIVE PAGE
-const [page, setPage] =
-useState("acid");
+  // ACTIVE PAGE
+  const [page, setPage] = useState("acid");
 
-return (
-<div className="app">
-{/* ACID & BASE */}
-{page === "acid" && (
-<AcidBase />
-)}
+  return (
+    <div className="app">
+      {/* ACID & BASE */}
+      {page === "acid" && <AcidBase />}
 
-{/* MOLARITY */}  
-  {page === "molarity" && (  
-    <Molarity />  
-  )}  
+      {/* MOLARITY */}
+      {page === "molarity" && <Molarity />}
 
-  {/* STOCK */}  
-  {page === "stock" && (  
-    <StockDilution />  
-  )}  
+      {/* STOCK */}
+      {page === "stock" && <StockDilution />}
 
-  {/* SOLUTIONS */}  
-  {page === "solutions" && (  
-    <Solutions />  
-  )}  
+      {/* SOLUTIONS */}
+      {page === "solutions" && <Solutions />}
 
-  {/* BOTTOM NAV */}  
-  <BottomNav  
-    page={page}  
-    setPage={setPage}  
-  />  
-</div>
+      {/* INDICATORS (ADDED) */}
+      {page === "indicators" && <Indicators />}
 
-);
+      {/* BOTTOM NAV */}
+      <BottomNav page={page} setPage={setPage} />
+    </div>
+  );
 }
 
 export default App;
-
