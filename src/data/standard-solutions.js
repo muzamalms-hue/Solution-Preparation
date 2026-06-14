1e7ff683f3aee1cd7acc1f4194d79f076e041ece
muzamalms-hue <muzamalms@gmail.com>
2026-06-14 14:21:13 +0500

    Update standard-solutions.js

--- src/data/standard-solutions.js
+++ src/data/standard-solutions.js
@@ -1,1 +1,373 @@
-
+[
+  {
+    "name": "Ammonium Standard Solution (100 ppm NH4)",
+    "strength": "100 ppm NH4",
+    "formula": "Ready-to-use standard",
+    "preparation": "Immediately before use, dilute to 25 mL with water R, 10 mL of a solution containing ammonium chloride R equivalent to 0.741 g NH4Cl in 1000 mL.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Ammonium Standard Solution (3 ppm NH4)",
+    "strength": "3 ppm NH4",
+    "formula": "Ready-to-use standard",
+    "preparation": "Immediately before use, dilute 100-fold with water R a solution containing ammonium chloride R equivalent to 0.889 g NH4Cl in 1000 mL.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Ammonium Standard Solution (2.5 ppm NH4)",
+    "strength": "2.5 ppm NH4",
+    "formula": "Ready-to-use standard",
+    "preparation": "Immediately before use, dilute 100-fold with water R a solution containing ammonium chloride R equivalent to 0.741 g NH4Cl in 1000 mL.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Ammonium Standard Solution (1 ppm NH4)",
+    "strength": "1 ppm NH4",
+    "formula": "Ready-to-use standard",
+    "preparation": "Immediately before use, dilute Ammonium Standard Solution (2.5 ppm NH4) to 2.5 times its volume with water R.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Barium Standard Solution (0.1% Ba)",
+    "strength": "0.1% Ba",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dissolve barium chloride R equivalent to 0.178 g BaCl2·2H2O in distilled water and dilute to 100.0 mL.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Barium Standard Solution (50 ppm Ba)",
+    "strength": "50 ppm Ba",
+    "formula": "Ready-to-use standard",
+    "preparation": "Immediately before use, dilute 20-fold with distilled water a solution containing barium chloride R equivalent to 0.178 g BaCl2·2H2O in 100.0 mL.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Barium Standard Solution (2 ppm Ba)",
+    "strength": "2 ppm Ba",
+    "formula": "Ready-to-use standard",
+    "preparation": "Immediately before use, dilute Barium Standard Solution (50 ppm Ba) 25-fold with distilled water.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Calcium Standard Solution (1000 ppm Ca)",
+    "strength": "1000 ppm Ca",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dissolve 2.5 g calcium carbonate in 23 mL of 1 M hydrochloric acid and make to 100 mL with distilled water. Immediately before use dilute 1 volume to 10 volumes.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Calcium Standard Solution (400 ppm Ca)",
+    "strength": "400 ppm Ca",
+    "formula": "Ready-to-use standard",
+    "preparation": "Immediately before use, dilute 10-fold with distilled water a solution containing calcium carbonate equivalent to 1.000 g CaCO3 and 23 mL of 1 M HCl in 100 mL.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Calcium Standard Solution (100 ppm Ca)",
+    "strength": "100 ppm Ca",
+    "formula": "Ready-to-use standard",
+    "preparation": "Immediately before use, dilute 10-fold with distilled water a solution containing calcium carbonate equivalent to 0.624 g CaCO3 and 3 mL acetic acid in 250 mL.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Calcium Standard Solution (10 ppm Ca)",
+    "strength": "10 ppm Ca",
+    "formula": "Ready-to-use standard",
+    "preparation": "Immediately before use, dilute 100-fold with distilled water a solution containing calcium carbonate equivalent to 0.624 g CaCO3 and 3 mL acetic acid in 250 mL.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Chloride Standard Solution (5 ppm Cl)",
+    "strength": "5 ppm Cl",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dilute 1 volume of 0.0824% w/v sodium chloride solution to 100 volumes with water immediately before use.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Chloride Standard Solution (8 ppm Cl)",
+    "strength": "8 ppm Cl",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dilute 1 volume of 0.132% w/v sodium chloride solution to 100 volumes with water immediately before use.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Chloride Standard Solution (50 ppm Cl)",
+    "strength": "50 ppm Cl",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dilute 1 volume of 0.0824% w/v sodium chloride solution to 10 volumes with water immediately before use.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Chromium Standard Solution (0.1% Cr)",
+    "strength": "0.1% Cr",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dissolve potassium dichromate equivalent to 2.83 g K2Cr2O7 in water and dilute to 1000.0 mL.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Chromium Standard Solution (100 ppm Cr)",
+    "strength": "100 ppm Cr",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dissolve potassium dichromate equivalent to 0.283 g K2Cr2O7 in water and dilute to 1000.0 mL.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Chromium Standard Solution (0.1 ppm Cr)",
+    "strength": "0.1 ppm Cr",
+    "formula": "Ready-to-use standard",
+    "preparation": "Immediately before use, dilute Chromium Standard Solution (100 ppm Cr) 1000-fold with water.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Cobalt Standard Solution (100 ppm Co)",
+    "strength": "100 ppm Co",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dissolve cobalt nitrate equivalent to 0.494 g Co(NO3)2·6H2O in 500 mL of 1 M nitric acid and dilute to 1000 mL with water.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Formaldehyde Standard Solution (5 ppm CH2O)",
+    "strength": "5 ppm CH2O",
+    "formula": "Ready-to-use standard",
+    "preparation": "Immediately before use, dilute 200-fold with water a solution containing 1.0 g CH2O per liter prepared from formaldehyde solution.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Hydrogen Peroxide Standard Solution (10 ppm H2O2)",
+    "strength": "10 ppm H2O2",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dilute 10.0 mL dilute hydrogen peroxide solution to 300.0 mL with water. Then dilute 10.0 mL of this solution to 1000.0 mL with water. Prepare immediately before use.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Iodide Standard Solution (20 ppm I)",
+    "strength": "20 ppm I",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dilute 10.0 mL of a 0.026% w/v potassium iodide solution to 100.0 mL with water.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Iodide Standard Solution (10 ppm I)",
+    "strength": "10 ppm I",
+    "formula": "Ready-to-use standard",
+    "preparation": "Immediately before use, dilute 100-fold with water a solution containing potassium iodide equivalent to 0.131 g KI in 100.0 mL.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Iron Standard Solution (0.1% Fe)",
+    "strength": "0.1% Fe",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dissolve 0.10 g iron in the minimum quantity of a mixture of equal volumes of hydrochloric acid and water, and dilute to 100 mL with water.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Iron Standard Solution (1 ppm Fe)",
+    "strength": "1 ppm Fe",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dilute 1 volume of Iron Standard Solution (20 ppm Fe) to 20 volumes with water immediately before use.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Iron Standard Solution (2 ppm Fe)",
+    "strength": "2 ppm Fe",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dilute 1 volume of Iron Standard Solution (20 ppm Fe) to 10 volumes with water immediately before use.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Iron Standard Solution (8 ppm Fe)",
+    "strength": "8 ppm Fe",
+    "formula": "Ready-to-use standard",
+    "preparation": "Immediately before use, dilute 10-fold with water a solution containing 80 mg iron and 50 mL hydrochloric acid (220 g/L HCl) in 1000.0 mL.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Iron Standard Solution (10 ppm Fe)",
+    "strength": "10 ppm Fe",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dissolve 7.022 g ammonium iron(II) sulfate in water containing 25 mL of 1 M sulfuric acid and dilute to 1000.0 mL. Dilute 1 volume to 100 volumes with water.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Iron Standard Solution (20 ppm Fe)",
+    "strength": "20 ppm Fe",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dissolve 0.863 g ammonium iron(II) sulfate in water containing 25 mL of 1 M sulfuric acid and dilute to 500 mL. Dilute 1 volume to 10 volumes with water immediately before use.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Iron Standard Solution (250 ppm Fe)",
+    "strength": "250 ppm Fe",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dilute 1 volume of a 4.84% w/v iron(III) chloride hexahydrate solution in 15.0% w/v hydrochloric acid to 40 volumes with water immediately before use.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Lead Standard Solution (1 ppm Pb)",
+    "strength": "1 ppm Pb",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dilute 1 volume of Lead Standard Solution (10 ppm Pb) to 10 volumes with water immediately before use.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Lead Standard Solution (2 ppm Pb)",
+    "strength": "2 ppm Pb",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dilute 1 volume of Lead Standard Solution (10 ppm Pb) to 5 volumes with water immediately before use.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Lead Standard Solution (0.1% Pb)",
+    "strength": "0.1% Pb",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dissolve 0.400 g lead(II) nitrate in sufficient water and dilute to 250.0 mL.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Lead Standard Solution (10 ppm Pb) R1",
+    "strength": "10 ppm Pb",
+    "formula": "Ready-to-use standard",
+    "preparation": "Immediately before use, dilute 10-fold with water a solution containing 0.160 g lead nitrate in 100 mL water plus 1 mL lead-free nitric acid and dilute to 1000.0 mL.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Lead Standard Solution (10 ppm Pb) R2",
+    "strength": "10 ppm Pb",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dilute Lead Standard Solution (0.1% Pb) R1 to 100 times its volume with dilute lead-free nitric acid. Use within 1 week.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Lead Standard Solution (10 ppm Pb)",
+    "strength": "10 ppm Pb",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dilute 1 volume of Lead Standard Solution (100 ppm Pb) to 10 volumes with water immediately before use.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Lead Standard Solution (100 ppm Pb)",
+    "strength": "100 ppm Pb",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dilute 1 volume of Lead Standard Solution (0.1% Pb) to 10 volumes with water immediately before use.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Lead Standard Solution (0.5 ppm Pb)",
+    "strength": "0.5 ppm Pb",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dilute Lead Standard Solution (10 ppm Pb) R2 to 20 times its volume with dilute lead-free nitric acid. Use within 1 day.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Lead Standard Solution (0.25 ppm Pb)",
+    "strength": "0.25 ppm Pb",
+    "formula": "Ready-to-use standard",
+    "preparation": "Immediately before use, dilute Lead Standard Solution (1 ppm Pb) to 4 times its volume with water.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Lead Standard Solution (0.1 ppm Pb)",
+    "strength": "0.1 ppm Pb",
+    "formula": "Ready-to-use standard",
+    "preparation": "Immediately before use, dilute Lead Standard Solution (1 ppm Pb) to 10 times its volume with water.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Magnesium Standard Solution (0.1% Mg)",
+    "strength": "0.1% Mg",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dissolve magnesium sulfate equivalent to 1.010 g MgSO4·7H2O in distilled water and dilute to 100.0 mL.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Magnesium Standard Solution (1000 ppm Mg)",
+    "strength": "1000 ppm Mg",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dissolve 5.275 g magnesium nitrate in 16 mL dilute nitric acid and dilute to 500.0 mL with water.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Nitrate Standard Solution (100 ppm NO3)",
+    "strength": "100 ppm NO3",
+    "formula": "Ready-to-use standard",
+    "preparation": "Immediately before use, dilute 10-fold with water a solution containing potassium nitrate equivalent to 0.815 g KNO3 in 500.0 mL.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Phosphate Standard Solution (200 ppm PO4)",
+    "strength": "200 ppm PO4",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dissolve potassium dihydrogen phosphate equivalent to 0.286 g KH2PO4 in water and dilute to 1000.0 mL.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Phosphate Standard Solution (100 ppm PO4)",
+    "strength": "100 ppm PO4",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dilute 10.0 mL of a 0.143% w/v potassium dihydrogen orthophosphate solution to 100.0 mL with water immediately before use.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Phosphate Standard Solution (5 ppm PO4)",
+    "strength": "5 ppm PO4",
+    "formula": "Ready-to-use standard",
+    "preparation": "Immediately before use, dilute 100-fold with water a solution containing potassium dihydrogen phosphate equivalent to 0.716 g KH2PO4 in 1000.0 mL.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Silver Standard Solution (5 ppm Ag)",
+    "strength": "5 ppm Ag",
+    "formula": "Ready-to-use standard",
+    "preparation": "Immediately before use, dilute 100-fold with water a solution containing silver nitrate equivalent to 0.790 g AgNO3 in 1000.0 mL.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Sodium Standard Solution (1000 ppm Na)",
+    "strength": "1000 ppm Na",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dissolve an amount of anhydrous sodium carbonate equivalent to 2.305 g Na2CO3 in a mixture of 25 mL water and 25 mL nitric acid, then dilute to 1000.0 mL with water.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Sodium Standard Solution (200 ppm Na)",
+    "strength": "200 ppm Na",
+    "formula": "Ready-to-use standard",
+    "preparation": "Immediately before use, dilute 10-fold with water a solution containing sodium chloride equivalent to 0.509 g NaCl in 100.0 mL.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Sodium Standard Solution (50 ppm Na)",
+    "strength": "50 ppm Na",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dilute Sodium Standard Solution (200 ppm Na) to 4 times its volume with water.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Sulphate Standard Solution (100 ppm SO4)",
+    "strength": "100 ppm SO4",
+    "formula": "Ready-to-use standard",
+    "preparation": "Immediately before use, dilute 10-fold with distilled water a solution containing dipotassium sulphate equivalent to 0.181 g K2SO4 in 100.0 mL.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Zinc Standard Solution (100 ppm Zn)",
+    "strength": "100 ppm Zn",
+    "formula": "Ready-to-use standard",
+    "preparation": "Immediately before use, dilute 10-fold with water a solution containing zinc sulphate equivalent to 0.440 g ZnSO4·7H2O and 1 mL acetic acid in 100.0 mL.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Zinc Standard Solution (25 ppm Zn)",
+    "strength": "25 ppm Zn",
+    "formula": "Ready-to-use standard",
+    "preparation": "Dilute 25.0 mL of Zinc Standard Solution (100 ppm Zn) to 100.0 mL with water immediately before use.",
+    "standardization": "Not required."
+  },
+  {
+    "name": "Zinc Standard Solution (10 ppm Zn)",
+    "strength": "10 ppm Zn",
+    "formula": "Ready-to-use standard",
+    "preparation": "Immediately before use, dilute Zinc Standard Solution (100 ppm Zn) 10-fold with water.",
+    "standardization": "Not required."
+  }
+]
