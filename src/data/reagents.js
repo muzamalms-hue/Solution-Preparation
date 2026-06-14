0254d57a0bdbfb7c58aac830aecf94f18c3f33ca
muzamalms-hue <muzamalms@gmail.com>
2026-06-14 19:09:21 +0500

    Update reagents.js

--- src/data/reagents.js
+++ src/data/reagents.js
@@ -1,1 +1,189 @@
+const reagents = [
+  {
+    name: "Acetic Acid, Dilute",
+    preparation:
+      "Dilute 12 g of glacial acetic acid to 100 ml with water. It contains not less than 11.5% and not more than 12.5% w/v of C2H4O2 (2M)."
+  },
+  {
+    name: "Acetic Anhydride Solution R1",
+    preparation:
+      "Dissolve 25 ml of acetic anhydride in anhydrous pyridine and dilute to 100 ml with anhydrous pyridine. Store protected from light and air."
+  },
+  {
+    name: "Ammonia Concentrated",
+    preparation:
+      "Use commercial ammonia solution (35% or 25% w/w NH3). Store properly and use as specified in procedures."
+  },
+  {
+    name: "Ammonia R1 Concentrated",
+    preparation:
+      "Concentrated ammonia containing not less than 32% w/w NH3 (about 18M). Store below 20°C protected from CO2."
+  },
+  {
+    name: "Ammonia R1 Dilute",
+    preparation:
+      "Dilute 41 g of concentrated ammonia to 100 ml with water. Contains about 6M NH3."
+  },
+  {
+    name: "Ammonia R2 Dilute",
+    preparation:
+      "Dilute 14 g of concentrated ammonia to 100 ml with water. Contains about 2M NH3."
+  },
+  {
+    name: "Ammonia R3 Dilute",
+    preparation:
+      "Dilute 0.7 g of concentrated ammonia to 100 ml with water."
+  },
+  {
+    name: "Ammonium Acetate Solution",
+    preparation:
+      "Dissolve 150 g ammonium acetate in water, add 3 ml glacial acetic acid and dilute to 1000 ml. Use within 1 week."
+  },
+  {
+    name: "Ammonium Carbonate Solution R1",
+    preparation:
+      "Dissolve 20 g ammonium carbonate in dilute ammonia and dilute to 100 ml with water."
+  },
+  {
+    name: "Ammonium Carbonate Solution Dilute",
+    preparation:
+      "Dissolve 5 g ammonium carbonate in ammonia and water mixture, dilute to 100 ml."
+  },
+  {
+    name: "Ammonium Iron(III) Sulphate Solution R1",
+    preparation:
+      "Dissolve 0.2 g ammonium iron(III) sulphate in water, add nitric acid and dilute to 100 ml."
+  },
+  {
+    name: "Barium Chloride Solution R1",
+    preparation:
+      "A 6.1% w/v solution of barium chloride."
+  },
+  {
+    name: "Barium Chloride Solution R2",
+    preparation:
+      "A 3.65% w/v solution of barium chloride."
+  },
+  {
+    name: "Barium Chloride Solution",
+    preparation:
+      "A 10% w/v solution of barium chloride."
+  },
+  {
+    name: "Bromine Water",
+    preparation:
+      "Shake bromine with water and allow to stand to form a saturated solution protected from light."
+  },
+  {
+    name: "Bromothymol Blue Solution R1",
+    preparation:
+      "Dissolve bromothymol blue in sodium hydroxide and ethanol, then dilute to 100 ml with water."
+  },
+  {
+    name: "Cerium(III) Nitrate Solution",
+    preparation:
+      "Dissolve cerium(III) nitrate in water, add nitric acid and hydroxylamine hydrochloride, then dilute to 1000 ml."
+  },
+  {
+    name: "Copper Sulphate Solution",
+    preparation:
+      "A 12.5% w/v solution of copper(II) sulphate."
+  },
+  {
+    name: "Diphenylamine Solution R1",
+    preparation:
+      "A 1% w/v solution of diphenylamine in sulphuric acid."
+  },
+  {
+    name: "Hydrochloric Acid, Dilute",
+    preparation:
+      "Dilute hydrochloric acid with water to required concentration (~7.3% w/v HCl)."
+  },
+  {
+    name: "Hydrochloric Acid R1",
+    preparation:
+      "Dilute hydrochloric acid to 25% w/v HCl with water."
+  },
+  {
+    name: "Iodine Solution R1",
+    preparation:
+      "Mix iodine with potassium iodide and dilute with water. Prepare fresh before use."
+  },
+  {
+    name: "Lead Nitrate Solution",
+    preparation:
+      "A 3.3% w/v solution of lead(II) nitrate."
+  },
+  {
+    name: "Mercury(II) Acetate Solution",
+    preparation:
+      "A 3.19% w/v solution of mercury(II) acetate in acetic acid, neutralized if required."
+  },
+  {
+    name: "Mercury(II) Chloride Solution",
+    preparation:
+      "A 5.4% w/v solution of mercury(II) chloride."
+  },
+  {
+    name: "Methanol R1",
+    preparation:
+      "Spectrophotometric grade methanol with specified UV transmittance."
+  },
+  {
+    name: "Methanol R2",
+    preparation:
+      "LC grade methanol containing not less than 99.8% purity."
+  },
+  {
+    name: "Ninhydrin Solution R1",
+    preparation:
+      "Dissolve ninhydrin in ethanol and glacial acetic acid."
+  },
+  {
+    name: "Nitric Acid, Dilute",
+    preparation:
+      "Dilute nitric acid with water to about 12.5% w/v HNO3."
+  },
+  {
+    name: "Perchloric Acid Solution",
+    preparation:
+      "Dilute perchloric acid with water to about 1M solution."
+  },
+  {
+    name: "Potassium Chromate Solution",
+    preparation:
+      "A 5% w/v solution of potassium chromate."
+  },
+  {
+    name: "Potassium Dichromate Solution",
+    preparation:
+      "A 10.6% w/v solution of potassium dichromate."
+  },
+  {
+    name: "Potassium Permanganate Solution",
+    preparation:
+      "A 3% w/v solution of potassium permanganate."
+  },
+  {
+    name: "Sodium Hydroxide Solution",
+    preparation:
+      "A 20% w/v sodium hydroxide solution, standardized by titration."
+  },
+  {
+    name: "Sodium Chloride Solution",
+    preparation:
+      "A 20% w/v solution of sodium chloride."
+  },
+  {
+    name: "Sulphuric Acid, Dilute",
+    preparation:
+      "Add sulphuric acid to water and dilute to 100 ml (~1M H2SO4)."
+  },
+  {
+    name: "Water R1",
+    preparation:
+      "Highly purified distilled water with conductivity ≤1 µS/cm."
+  }
+];
 
+export default reagents;
