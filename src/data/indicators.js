cd47fb21647288e5feb520fa26f1b941a145ee6b
muzamalms-hue <muzamalms@gmail.com>
2026-06-14 17:33:08 +0500

    Update indicators.js

--- src/data/indicators.js
+++ src/data/indicators.js
@@ -1,1 +1,126 @@
+// data/indicators.js
 
+const indicators = [
+  {
+    name: "Bromophenol Blue Solution",
+    preparation:
+      "Dissolve 0.1 g bromophenol blue in 1.5 ml of 0.1M sodium hydroxide and 20 ml ethanol (96%). Add water to make 100 ml."
+  },
+  {
+    name: "Bromothymol Blue Solution R1",
+    preparation:
+      "Dissolve 50 mg bromothymol blue in 4 ml 0.02M sodium hydroxide and 20 ml ethanol (96%). Dilute to 100 ml with water."
+  },
+  {
+    name: "BRP Indicator Solution",
+    preparation:
+      "Dissolve 0.1 g bromothymol blue, 20 mg methyl red, and 0.2 g phenolphthalein in ethanol (96%) to make 100 ml. Filter."
+  },
+  {
+    name: "Crystal Violet Solution",
+    preparation:
+      "Prepare 0.5% w/v solution of crystal violet in anhydrous acetic acid."
+  },
+  {
+    name: "Eosin Y",
+    preparation:
+      "1 g dissolves in 2 ml water or 50 ml alcohol."
+  },
+  {
+    name: "Eriochrome Black T Trituration",
+    preparation:
+      "Grind 200 mg eriochrome black T with 20 g potassium chloride to a fine powder."
+  },
+  {
+    name: "Ferroin Solution",
+    preparation:
+      "Dissolve 0.7 g iron(II) sulphate and 1.76 g phenanthroline hydrochloride in water and dilute to 100 ml."
+  },
+  {
+    name: "Methyl Orange Solution",
+    preparation:
+      "Dissolve 0.1 g methyl orange in 80 ml water and add ethanol (96%) to make 100 ml."
+  },
+  {
+    name: "Methyl Red Solution",
+    preparation:
+      "Dissolve 50 mg methyl red in 1.86 ml 0.1M sodium hydroxide and 50 ml ethanol (96%). Add water to 100 ml."
+  },
+  {
+    name: "Mordant Black 11 Mixed Triturate",
+    preparation:
+      "Mix 1 g mordant black 11, 0.4 g methyl orange, and 100 g sodium chloride."
+  },
+  {
+    name: "Mordant Black 11 Triturate",
+    preparation:
+      "Mix 1 part mordant black 11 with 99 parts sodium chloride."
+  },
+  {
+    name: "Oracet Blue B Solution",
+    preparation:
+      "Prepare 0.5% w/v solution of oracet blue B in anhydrous acetic acid."
+  },
+  {
+    name: "Phenol Red Solution",
+    preparation:
+      "Dissolve 0.1 g phenol red in 2.82 ml 0.1M sodium hydroxide and ethanol (96%). Add water to 100 ml."
+  },
+  {
+    name: "Phenol Red Solution R1 (Buffered)",
+    preparation:
+      "Mix phenol red sodium hydroxide solution with acetic acid/sodium hydroxide buffer as specified."
+  },
+  {
+    name: "Phenolphthalein Solution",
+    preparation:
+      "Dissolve 0.1 g phenolphthalein in 80 ml alcohol and dilute to 100 ml with water."
+  },
+  {
+    name: "Phenolphthalein Solution R1",
+    preparation:
+      "Prepare 1.0% w/v phenolphthalein in ethanol (96%)."
+  },
+  {
+    name: "Potassium Iodide and Starch Solution",
+    preparation:
+      "Dissolve potassium iodide in water, add starch solution, boil and cool."
+  },
+  {
+    name: "Solochrome Dark Blue Mixture",
+    preparation:
+      "Mix solochrome dark blue with sodium sulphate (1:99 ratio)."
+  },
+  {
+    name: "Starch Iodate Paper",
+    preparation:
+      "Soak filter paper in starch + potassium iodate solution, dry protected from light."
+  },
+  {
+    name: "Starch Iodide Paper",
+    preparation:
+      "Soak filter paper in starch + potassium iodide solution, dry protected from light."
+  },
+  {
+    name: "Starch Iodide Solution",
+    preparation:
+      "Dissolve starch and potassium iodide in water."
+  },
+  {
+    name: "Thymol Blue Solution",
+    preparation:
+      "Dissolve 0.1 g thymol blue in 2.15 ml 0.1M sodium hydroxide and ethanol (96%). Dilute to 100 ml."
+  },
+  {
+    name: "Starch Mucilage",
+    preparation:
+      "Triturate starch with water, boil, cool and filter. Prepare freshly."
+  },
+  {
+    name: "Xylenol Orange Solution",
+    preparation:
+      "Shake 0.1 g xylenol orange in 100 ml water and filter if needed."
+  }
+];
+
+export default indicators;
